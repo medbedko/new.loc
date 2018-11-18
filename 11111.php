fd860f6307e3812a8ba027345ad64f46af045e2d
Egor Puzikov <medbedko1@rambler.ru>
2018-11-18 22:53:40 +0300

    test
    test

--- 11111.php
+++ 11111.php
@@ -3,5 +3,5 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Title");
 ?>
 Text here....
-
+"hello111111"
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
